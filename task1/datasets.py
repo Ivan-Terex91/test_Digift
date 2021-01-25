d43bb2c424d3253c49fa214996f2b281c7c71750
Ivan Terehin <ITerex91@yandex.ru>
2021-01-25 11:23:10 +0300

    Поправил ошибку в первом задании, последний символ строки декодировался дважды

--- task1/datasets.py
+++ task1/datasets.py
@@ -3,10 +3,10 @@ dictionary = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9:
               24: 'x', 25: 'y', 26: 'z'}
 
 string1 = "12314"
-expected_result1 = ['a', 'l', 'b', 'w', 'c', 'a', 'n', 'd', 'd']
+expected_result1 = ['a', 'l', 'b', 'w', 'c', 'a', 'n', 'd']
 
 string2 = "12314123"
-expected_result2 = ['a', 'l', 'b', 'w', 'c', 'a', 'n', 'd', 'a', 'l', 'b', 'w', 'c', 'c']
+expected_result2 = ['a', 'l', 'b', 'w', 'c', 'a', 'n', 'd', 'a', 'l', 'b', 'w', 'c']
 
 first_dataset = (dictionary, string1, expected_result1)
 second_dataset = (dictionary, string2, expected_result2)
--- task1/task1.py
+++ task1/task1.py
@@ -1,12 +1,13 @@
 
 def test_decode_data(prepare_data):
-    """Декадируем данные string через dictionary"""
+    """Декодируем данные string через dictionary"""
     dictionary, string, expected_result = prepare_data
     received_result = list()
     while string:
         temp = string[:2]
         received_result.append(dictionary.get(int(temp[0])))
-        received_result.append(dictionary.get(int(temp))) if int(temp) in dictionary else None
+        if len(temp) == 2:
+            received_result.append(dictionary.get(int(temp))) if int(temp) in dictionary else None
         string = string[1:]
 
     assert received_result == expected_result, f"Expected {expected_result} \n got {received_result} "
